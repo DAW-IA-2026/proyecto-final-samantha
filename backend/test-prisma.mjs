8140bde7c60d6c7b609e6eb4fcd81a8b558f175c
Sam <usagibakura@gmail.com>
2026-05-31 19:26:53 +0200

    primer-sprint

--- backend/test-prisma.mjs
+++
@@ -1,6 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-await prisma.$connect();
-console.log('prisma connected');
-import http from 'http';
-http.createServer((req,res)=>res.end('ok')).listen(4001, ()=>console.log('up'));
